940e1dceb0c1f600463a1c7ade81c242ce874400
Daniel-Rakipllari <danielrakipllari@gmail.com>
2023-04-27 23:04:40 +0200

    channge pass

--- Authentication/emailSender.php
+++ Authentication/emailSender.php
@@ -40,7 +40,7 @@ try {
     $mail->Username = 'onlineclothingstore02@outlook.com';
 
     // SMTP password
-    $mail->Password = 'BalenamoD01';
+    $mail->Password = 'test';
 
     // Set who the message is to be sent from
     $mail->setFrom('onlineclothingstore02@outlook.com', 'Your Name');
